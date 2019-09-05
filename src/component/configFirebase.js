f6e3b63f3b97e5a632ead8793eea0dc8db4161c3
infobinod <veenodbanstola@gmail.com>
2019-09-05 09:30:37 +0545

    this file is modified

--- src/component/configFirebase.js
+++ src/component/configFirebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 const config={
   
-    apiKey: "AIzaSyCJ5dKQkzXIYjY-LAJalr3CwBdgsgblOU0",
-    authDomain: "batti-44e50.firebaseapp.com",
-    databaseURL: "https://batti-44e50.firebaseio.com",
-    projectId: "batti-44e50",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "472704246303",
-    appId: "1:472704246303:web:493459fb380a8703"
+    messagingSenderId: "",
+    appId: ""
   
 
 };  
